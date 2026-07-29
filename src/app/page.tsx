f702613b20aba945de92a849b91aef5d1ec44afe
Venicius Rocha Dobler <venicius@taktico.com.br>
2026-07-29 15:37:22 -0300

    feat: parallax de scroll no hero e adiciona seção Sistema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/Hero";
+import Sistema from "@/components/sections/Sistema";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Sistema />
     </main>
   );
 }
